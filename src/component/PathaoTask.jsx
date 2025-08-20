fc89038b3140385b21dec5c61baee10e8cf6c1b2
Rafiqul Hasan <rafiqulhasan398@gmail.com>
2025-08-20 22:26:45 +0600

    full display horizontal and vertical line drow created

--- src/component/PathaoTask.jsx
+++ src/component/PathaoTask.jsx
@@ -1,8 +1,29 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const PathaoTask = () => {
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const drawCrosshair = useCallback(() => {
+    const ctx = ctxRef.current;
+    if (!ctx) return;
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+    // Y-axis
+    ctx.beginPath();
+    ctx.moveTo(mousePosition.x, 0);
+    ctx.lineTo(mousePosition.x, canvasRef.current?.height || 0);
+    ctx.stroke();
+    // X-axis
+    ctx.beginPath();
+    ctx.moveTo(0, mousePosition.y);
+    ctx.lineTo(canvasRef.current?.width, mousePosition.y || 0);
+    ctx.stroke();
+  }, [mousePosition]);
 
   const drawAll = useCallback(() => {
     const ctx = ctxRef.current;
@@ -11,7 +32,8 @@ const PathaoTask = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#99c2ff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, []);
+    drawCrosshair();
+  }, [drawCrosshair]);
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
@@ -19,8 +41,12 @@ const PathaoTask = () => {
     ctxRef.current = ctx;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    setMousePosition({
+      x: canvas.width / 2,
+      y: canvas.height / 2,
+    });
     drawAll();
-  }, []);
+  }, [drawAll]);
   return <canvas ref={canvasRef} />;
 };
 
