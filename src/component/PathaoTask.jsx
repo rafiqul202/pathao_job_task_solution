d885638ab3a98d031bbc47bf1ecc095d8ef6a3fc
Rafiqul Hasan <rafiqulhasan398@gmail.com>
2025-08-21 09:56:03 +0600

    draw x or y state position change and move to mouse cursor pointer section complited

--- src/component/PathaoTask.jsx
+++ src/component/PathaoTask.jsx
@@ -3,6 +3,18 @@ import { useCallback, useEffect, useRef, useState } from "react";
 const PathaoTask = () => {
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
+  const drawingStateRef = useRef({
+    isDrawing: false,
+    isDragging: false,
+    startX: 0,
+    startY: 0,
+    dragOffsetX: 0,
+    dragOffsetY: 0,
+    selectRect: null,
+    clickStartTime: 0,
+    hasMove: false,
+    fillColor : "gray"
+  })
   const [mousePosition, setMousePosition] = useState({
     x: 0,
     y: 0,
@@ -46,8 +58,39 @@ const PathaoTask = () => {
       y: canvas.height / 2,
     });
     drawAll();
+  }, []);
+
+  useEffect(() => {
+    drawAll();
   }, [drawAll]);
-  return <canvas ref={canvasRef} />;
+
+  const getMousePosition = useCallback((e) => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return {
+        x: 0,
+        y: 0,
+      };
+    }
+    const rect = canvas.getBoundingClientRect();
+    // console.log(rect);
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    };
+  }, []);
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      const { x, y } = getMousePosition(event);
+      // console.log("henleMouseMove X", x);
+      // console.log("henleMouseMove Y", y);
+      setMousePosition({ x, y });
+      const state = drawingStateRef.current;
+    },
+    [getMousePosition]
+  );
+  return <canvas onMouseMove={handleMouseMove} ref={canvasRef} />;
 };
 
 export default PathaoTask;
